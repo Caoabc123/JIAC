f32f97ad865fcb38a92d6a88c8c6eb59b1009256
my-username-cao-on-github <55875337+Caoabc123@users.noreply.github.com>
2020-03-05 14:14:28 +0100

    gör ! på logotypen

--- www/js/Kontaktaoss.js
+++ www/js/Kontaktaoss.js
@@ -6,7 +6,7 @@ class Kontaktaoss extends Base {
         <div class="col-12">
           <h1>Kontakta oss</h1>
           <p>T1.</p>
-          <h2> This is DHYR & RUMSON.</p>
+          <h2> This is DHYR & RUMSON!</p>
           <h3> Here we insert images</h3>
           <p3> Nu vill jag pröva om detta hittas <br> och vi vet inte vad man gör och hur vi gör det.<p3>
           </div>
